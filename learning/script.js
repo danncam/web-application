8241500bb81bebcdb47370e62ab6381be110df7a
Daniel Neves <danielncamargo@gmail.com>
2019-10-03 21:57:02 -0300

    learning/js

---
+++ learning/script.js
@@ -0,0 +1,15 @@
+function mudarCor(novaCor) {
+    var elemento = document.getElementById("para1");
+    elemento.style.color = novaCor;
+    console.log(novaCor);
+}
+
+function getname() {
+    var instr = document.getElementById("form_id").value;// Form submission
+    alert(instr);
+}
+
+function submitform() {
+    var usrlogin = document.getElementById("login_id");
+}
+
